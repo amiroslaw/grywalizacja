8642922163c6287cf5a332a1a8d0e8ed2285ac8a
miro <arek47@gmail.com>
2016-02-26 21:18:55 +0100

    Dodanie brakujących plików

---
+++ src/application/CardController.java
@@ -0,0 +1,154 @@
+package application;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+public class CardController implements Initializable {
+
+	SampleController controller = new SampleController();
+	// Talia talia = new Talia();
+	@FXML
+	private Label lblnrKarty;
+	@FXML
+	private Label lblTyp;
+	@FXML
+	private TextField txtNazwa;
+	@FXML
+	private TextArea txtOpis;
+	@FXML
+	private Button btnObrazek;
+	@FXML
+	private Button btnDalej;
+	/**
+	 * typ karty
+	 */
+	File file;
+	String linkObrazek; 
+	int iKarta = 1;
+	@FXML
+	public void otworzObrazek(ActionEvent e){
+		FileChooser fileChooser= new FileChooser();
+		fileChooser.setTitle("wybierz obrazek");
+		fileChooser.setInitialDirectory(new File (System.getProperty("user.home")) );
+		fileChooser.getExtensionFilters().addAll(
+				new FileChooser.ExtensionFilter("JPG", "*.jpg"),
+				new FileChooser.ExtensionFilter("All file", "*.*")
+				);
+//	File file = new File(null);
+		file = fileChooser.showOpenDialog(new Stage());
+		if(file!=null){
+			// przypisanie ścieżki lub i skpiowanie
+			System.out.println(file.getAbsolutePath());
+//		karta.setObrazek(file.getAbsolutePath());
+	linkObrazek=file.getAbsolutePath();
+		}
+		//nie zadzaila bo musi byc klikniety button i nie wybrany obrazek aby sie aktywowal else
+//		else{ linkObrazek= "default"; }
+	}
+	@FXML
+	public void zapisz(ActionEvent e){
+		controller.zapisz(null);
+	}
+	/**
+	 * zapisuje kartę do talii i odświerza scene
+	 * @param e
+	 */
+	@FXML
+	public void nastepnaKarta(ActionEvent e) {
+		wyswietlTyp();
+		Karta karta = new Karta();
+		karta.setTytul(txtNazwa.getText());
+		karta.setOpis(txtOpis.getText());
+		if(file!=null){
+		karta.setObrazek(linkObrazek);
+		}else {
+		karta.setObrazek("default");
+		}
+		file=null;
+		// dodac metode czytajaca link do obrazka
+		switch (iKarta) {
+		case 1:
+//			lblTyp.setText(Integer.toString(1));
+			karta.setTyp(1);
+			break;
+		case 2:
+		case 3:
+		case 4:
+			karta.setTyp(2);
+			break;
+		default:
+//			lblTyp.setText(Integer.toString(3));
+			karta.setTyp(3);
+			break;
+		}
+		SampleController.talia.arrayTalia.add(karta);
+		iKarta++;
+			lblnrKarty.setText(Integer.toString(iKarta));
+//			txtNazwa.setText("");
+//			if(file!=null)
+			txtOpis.setText("");
+		if(iKarta==10){
+			btnDalej.setDisable(true);
+			controller.zapisz(null);
+		}
+			
+	}
+	/**
+	 * ustawianie laber typ gdyz jest opuzniony najpierw wyswietla sie startowy stage
+	 */
+	public void wyswietlTyp(){
+		if(iKarta>0 && iKarta<4)
+			lblTyp.setText(Integer.toString(2));
+		else
+			lblTyp.setText(Integer.toString(3));
+	}
+
+	@FXML
+	public void scenaLosuj(ActionEvent e) {
+		// zmienione na przejscie na start
+		StartController.goToStart();
+		try {
+//			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
+//			Main.windowLosuj.setScene(Main.scene);
+//			Main.windowLosuj.setTitle("Grywalizacja- losuj");
+//			Main.windowLosuj.setOnCloseRequest(event->controller.zapisz(null));
+//			Main.windowLosuj.show();
+
+		} catch (Exception ev) {
+			ev.printStackTrace();
+		}
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+//		for (int i = 0; i < SampleController.talia.arrayTalia.size(); i++) {
+//			System.out.println(SampleController.talia.arrayTalia.get(0));
+//		}
+		System.out.println("scena karta wczytanie");
+		// trzeba to zapisac do bd bo przy wczytaniu tamtej sceny czyta z bd
+		System.out.println("czy rozpoczęta "+SampleController.czyRozpoczeta);
+		SampleController.czyRozpoczeta = 1;
+		SampleController.talia.arrayTalia.clear();
+//		nastepnaKarta(null);
+	}
+	private Stage stage;
+	public void init(Stage stage) {
+	this.stage=stage;	
+	}
+
+}
---
+++ src/application/StartController.java
@@ -0,0 +1,86 @@
+package application;
+
+import java.io.IOException;
+		/**
+		 * 	layout poczatkowy
+		 * przelaczanie pomiedzy scenami 
+		 * mozna dodac elementy menu
+	
+		 */
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+
+public class StartController implements Initializable {
+	@FXML
+	private Button btnCreate; 
+	@FXML
+	private Button btnDraw; 
+	@FXML
+	private Label info; 
+	@FXML
+	void goToLos (){
+		try {
+//			FXMLLoader loader = new FXMLLoader(getClass().getResource("Sample.fxml"));
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
+		
+		 Main.scene = new Scene(root,400,600);
+		// scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		Main.windowLosuj.setScene(Main.scene);
+		Main.windowLosuj.setTitle("Grywalizacja- losuj");
+		Main.windowLosuj.show();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	@FXML
+	void goToCreate(){
+		try {
+			
+			GridPane layoutTalia = (GridPane) FXMLLoader.load(getClass().getResource("GeneratorKart.fxml")); ;
+			 Main.scene = new Scene(layoutTalia,400,400);
+			Main.windowLosuj.setScene(Main.scene);
+			Main.windowLosuj.setTitle("Grywalizacja-O tworzenie talii");
+			Main.windowLosuj.show();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+	}
+	static public void goToStart(){
+		VBox root;
+		try {
+			root = (VBox)FXMLLoader.load(StartController.class.getResource("Start.fxml"));
+		 Main.scene =new Scene(root, 400, 400);
+		 Main.windowLosuj.setScene(Main.scene);
+			Main.windowLosuj.setTitle("Grywalizacja-start");
+			Main.windowLosuj.show();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+//		
+//		Main.scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+//		primaryStage.setScene(scene);
+//		
+//		primaryStage.show()
+	}
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
